f127256b649e40f5e5fcd011a73445f9d9abc348
ayat <abuhajayat@gmail.com>
2024-12-27 16:26:25 +0100

    route

--- HiveBoard/src/App.jsx
+++ HiveBoard/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx';
 import HomePage from './pages/HomePage.jsx';
 import ErrorPage from './pages/ErrorPage.jsx';
+import LandingPage from './pages/LandingPage.jsx'
 import './App.css'
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path="/" element= {<HomePage/>} />
+        <Route path="/" element={<LandingPage />} /> 
+        <Route path="/home" element={<HomePage />} /> 
           <Route path="*" element={<ErrorPage/>} />
         </Routes>
       </div>
