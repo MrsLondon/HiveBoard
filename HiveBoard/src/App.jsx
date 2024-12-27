32fdc8a0f987dca8902d306932e5fe3b53368909
ayat <abuhajayat@gmail.com>
2024-12-27 15:40:35 +0100

    routes testing

--- HiveBoard/src/App.jsx
+++ HiveBoard/src/App.jsx
@@ -1,4 +1,6 @@
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx';
+import HomePage from './pages/HomePage.jsx';
 import './App.css'
 
 function App() {
@@ -6,6 +8,9 @@ function App() {
     <>
       <div className="App">
         <Navbar/>
+        <Routes>
+          <Route path="/" element= {<HomePage/>} />
+        </Routes>
       </div>
         
     </>
