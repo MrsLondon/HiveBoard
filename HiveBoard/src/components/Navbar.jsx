8872f8aeb8d134423e5701b0a724585148d4eeec
ayat <abuhajayat@gmail.com>
2024-12-27 21:45:40 +0100

    first land page

--- HiveBoard/src/components/Navbar.jsx
+++ HiveBoard/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     return (
       <nav className="Navbar">
         <div className="logo">
-          <img src="src/assets/hive.jpg" alt="App Logo" />
-          <span className="app-name">Your App Name</span>
+          <img src="src/assets/hive.png" alt="App Logo" />
+          <span className="app-name">Hive Board</span>
         </div>
         <ul>
             <li><Link to={"/home"}>Home</Link></li>
