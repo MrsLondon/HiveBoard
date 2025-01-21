915705703a01f737edc70a660920c7d82aeacb99
randa89 <randa.abusnaineh@gmail.com>
2025-01-21 18:22:48 +0100

    fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,48 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import HomeSidebar from '../components/HomeSidebar';
+import HomeSidebar from "../components/HomeSidebar";
 import BoardManager from "../components/BoardManager";
-import { BoardContext } from '../context/BoardContext';
-
+import { BoardContext } from "../context/BoardContext";
+import kanabanDate from "../kanban.json";
+import Header from "../components/Header";
 
 const Home = () => {
   const boardData = {
-    active:0,
-    boards:[
+    active: 0,
+    boards: [
       {
         //name:'To do list',
         //bgcolor:'#069',
-        list:[
-          {id:"1",title:"To do",items:[{id:"cdrFt",title:"Project Description 1"}]},
-          {id:"2",title:"In Progress",items:[{id:"cdrFv",title:"Project Description 2"}]},
-          {id:"3",title:"Done",items:[{id:"cdrFb",title:"Project Description 3"}]}
-        ]
-      }
-    ]
-  }
-  const [allboard,setAllBoard] = useState(boardData); 
-
+        list: [
+          {
+            id: "1",
+            title: "To do",
+            items: [{ id: "cdrFt", title: "Project Description 1" }],
+          },
+          {
+            id: "2",
+            title: "In Progress",
+            items: [{ id: "cdrFv", title: "Project Description 2" }],
+          },
+          {
+            id: "3",
+            title: "Done",
+            items: [{ id: "cdrFb", title: "Project Description 3" }],
+          },
+        ],
+      },
+    ],
+  };
+  const [allboard, setAllBoard] = useState(kanabanDate);
 
   return (
     <div>
-        <BoardContext.Provider value={{allboard,setAllBoard}}>
-      <div className='content flex'>
-         <HomeSidebar/> 
-         <BoardManager/> 
-      </div>
-        </BoardContext.Provider>
+      <BoardContext.Provider value={{ allboard, setAllBoard }}>
+        <div className="content flex">
+          <HomeSidebar />
+          <BoardManager />
+        </div>
+      </BoardContext.Provider>
     </div>
   );
 };
